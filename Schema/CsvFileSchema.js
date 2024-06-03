6dc4c43370f2c9bd040ccf1f79663e31a869f69d
Afif-Ur-Rahman <afifurrahman444@gmail.com>
2024-06-03 19:41:11 +0500

    Updated CSV Schema

--- Schema/CsvFileSchema.js
+++ Schema/CsvFileSchema.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
 const CsvSchema = new mongoose.Schema({
-  Year: String,
-  Value: String,
+  "First Name": {type: String, default: ""},
+  "Last Name": {type: String, default: ""},
+  "Job Title": {type: String, default: ""},
+  Company: {type: String, default: ""},
+  Email: {type: String, default: ""},
+  "Company Number": {type: String, default: ""},
+  Industry: {type: String, default: ""},
+  City: {type: String, default: ""},
+  Country: {type: String, default: ""},
 });
 
 const saveCsv = mongoose.model("csvdata", CsvSchema);
